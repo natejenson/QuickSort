85947bd96e5c1007fc3de6bf285ab80ea159865c
Nate Jenson <nate.m.jenson@gmail.com>
2016-11-25 14:51:06 -0600

    initial commit

---
+++ src/QuickSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickSort
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			int[] sample = {4, 2, 8, 5, 9, 4, 0, 3, 7};
+			Console.WriteLine($"Input:  {Stringify(sample)}");
+			Sorter.QuickSort(sample);
+			Console.WriteLine($"Output: {Stringify(sample)}");
+			Console.Read();
+		}
+
+		private static string Stringify<T>(IEnumerable<T> collection)
+		{
+			return string.Join(", ", collection);
+		}
+	}
+}
---
+++ src/QuickSort/Sorter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+	public class Sorter
+	{
+		public static void QuickSort(int[] arr)
+		{
+			QuickSort(arr, 0, arr.Length - 1);
+		}
+
+		private static void QuickSort(int[] arr, int low, int high)
+		{
+			
+		}
+	}
+}
