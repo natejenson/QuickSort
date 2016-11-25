ac6da4d1f3d42a78b5d484102f16df48943d52f1
Nate Jenson <nate.m.jenson@gmail.com>
2016-11-25 16:02:43 -0600

    implement quicksort

--- src/QuickSort/Sorter.cs
+++ src/QuickSort/Sorter.cs
@@ -9,17 +9,62 @@ namespace QuickSort
 	{
 		public static void QuickSort(int[] arr)
 		{
+			if (arr == null)
+			{
+				throw new ArgumentException("The input array cannot be null");
+			}
+			if (arr.Length <= 1)
+			{
+				return;
+			}
 			QuickSort(arr, 0, arr.Length - 1);
 		}
 
 		private static void QuickSort(int[] arr, int low, int high)
 		{
-			throw new NotImplementedException();
+			int partition = Partition(arr, low, high);
+			if (low < partition - 1)
+			{
+				QuickSort(arr, low, partition - 1);
+			}
+			if ( high > partition)
+			{
+				QuickSort(arr, partition, high);
+			}
 		}
 
 		private static int Partition(int[] arr, int low, int high)
 		{
-			throw new NotImplementedException();
+			int pivot = arr[low];
+			int left = low;
+			int right = high;
+			while (left <= right)
+			{
+				while (arr[left] < pivot)
+				{
+					left++;
+				}
+
+				while (arr[right] > pivot)
+				{
+					right--;
+				}
+
+				if (left <= right)
+				{
+					Swap(arr, left, right);
+					left++;
+					right--;
+				}
+			}
+			return left;
+		}
+
+		private static void Swap(int[] arr, int indexA, int indexB)
+		{
+			var temp = arr[indexA];
+			arr[indexA] = arr[indexB];
+			arr[indexB] = temp;
 		}
 	}
 }
